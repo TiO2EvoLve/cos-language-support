8e850a985ff51ce4fc5d3569adb8880acb00f297
TiO2 <3290158038@qq.com>
2025-06-06 11:38:48 +0800

    添加.gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -111,6 +111,8 @@ export function activate(context: vscode.ExtensionContext) {
                     createCompletionItem('@@CURRDATE', '当前日期 格式为yyyyMMdd'),
                     createCompletionItem('@@CURRDATETIMES', '当前时间日期 格式为yyyy-MM-dd HH:mm:ss'),
                     createCompletionItem('@@CURRDATES', '前日期 格式为yyyy-MM-dd'),
+                    createCompletionItem('CHECK.>', '检查'),
+                    createCompletionItem('END', '结束语')
                 ];
 
                 return completions;
